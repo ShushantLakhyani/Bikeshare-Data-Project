2049f740f19637f77c5a14a5fd3a4159bd44663b
Shushant Lakhyani <slakhyani20@gmail.com>
2021-10-02 06:27:17 +0530

    Changed a commit in the bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -19,7 +19,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data! \n')
-    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     #Get user input for any selected city
     while True:
         city = input("Please enter the city name. Choose a city from Chicago, New York or Washington \n").lower()
